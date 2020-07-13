968bb3a689b426d47cdb1895d00646dc127720fa
codershona <falgunimst95@gmail.com>
2020-07-13 17:11:46 +0200

    Sockett.io Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,18 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (count) => {
+
+	console.log('The count has been updated!', count)
+	
+})
 
 
+
+document.querySelector('#increment').addEventListener('click', () => {
+
+   console.log('Clicked')
+
+   socket.emit('increment')
+
+
+})
--- src/index.js
+++ src/index.js
@@ -19,9 +19,33 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+
+// server (emit) -> client (receive) - countUpdated
+
+// client (emit) -> server (receive) - increment
+
+
+
+let count = 0
+
+io.on('connection', (socket) => {
 
 	console.log('New WebSocket connection!')
+
+	socket.emit('countUpdated', count)
+
+	socket.on('increment', () => {
+		count++
+
+		// socket.emit('countUpdated', count)
+
+		io.emit('countUpdated', count)
+
+
+
+	})
+
+
 
 })
 
