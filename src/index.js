0c926f1aadb66e81165d1804296d1cd1019f205d
codershona <falgunimst95@gmail.com>
2020-07-14 17:16:51 +0200

    Broadcasting Events

--- src/index.js
+++ src/index.js
@@ -33,14 +33,22 @@ io.on('connection', (socket) => {
 
     socket.emit('message', 'Welcome!')
 
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+
+
     // socket.on('sendMessage', (message) => {
      socket.on('sendMessage', (message) => {
 
      	io.emit('message', message)
       
-
-
     })
+
+     socket.on('disconnect', () => {
+     
+       io.emit('message', 'A user has left!')
+
+     })
 
 
 	// socket.emit('countUpdated', count)
